371162c5d385584d3f0188183b513d261ed0e42a
David Lu <davidelu93@gmail.com>
2015-05-10 04:43:54 -0400

    add grunt buildcontorl

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,21 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: pkgConfig,
 
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:conundrumer/musicpsych.git',
+          branch: 'gh-pages'
+        }
+      },
+    },
+
     webpack: {
       options: webpackDistConfig,
       dist: {
